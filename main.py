f4d2ec83b21b6fede0964b89127a330396927aa1
Hendrik Tjabben <hendrik.tjabben@student.hpi.de>
2020-12-16 12:34:41 +0100

    Error handling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import multiprocessing
 from flask import Flask, render_template, request, json
+from typing import Dict
 from src.heatcontrol import get_temperature, turn_on_heating, turn_off_heating
 
 host='0.0.0.0'
@@ -28,25 +29,41 @@ def index():
         elif type == 'onoff':
             running.value = not running.value
             if running.value == True:
-                superviser.start()
+                if superviser == None:
+                    superviser = multiprocessing.Process(target=supervise, args=(temp_is, temp_should, running))
+                    try:
+                        superviser.start()
+                    except RuntimeError:
+                        superviser = None
+                        return create_json_response(
+                            response = {'success': False, 'reason': 'Process already running'},
+                            status = 500)
             else:
-                superviser.join()
-                superviser = multiprocessing.Process(target=supervise, args=(temp_is, temp_should, running))
+                try:
+                    superviser.join(timeout=5)
+                    if superviser is not None and superviser.is_alive():
+                        return create_json_response(
+                            response = {'success': False, 'reason': 'Failed to stop process.'},
+                            status = 500)
+                    superviser = None
+                except RuntimeError:
+                    return create_json_response(
+                        response = {'success': False, 'reason': 'Cannot stop. Process not running.'},
+                        status = 500)
 
-
-        return json.dumps({'status': 'OK'})
+        return create_json_response(
+            response = {'success': True},
+            status = 200
+        )
     elif request.method == 'GET':
         return render_template('main.html', temp_is=temp_is.value, temp_should=temp_should.value)
 
 @app.route('/get_status', methods=['GET'])
 def get_status():
     global temp_is, temp_should, running
-    response = app.response_class(
-        response=json.dumps({'success': True, 'temp_is': temp_is.value, 'temp_should': temp_should.value, 'running': running.value}),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+    return create_json_response(
+        response = {'success': True, 'temp_is': temp_is.value, 'temp_should': temp_should.value, 'running': running.value},
+        status = 200)
 
 def supervise(temp_is, temp_should, running):
     while(running.value):
@@ -57,16 +74,23 @@ def supervise(temp_is, temp_should, running):
             turn_off_heating()
         time.sleep(1)
 
+def create_json_response(response: Dict[str, object], status: int):
+    response = app.response_class(
+        response=json.dumps(response),
+        status=status,
+        mimetype='application/json'
+    )
+    return response
+
+
 def main():
     global temp_is, temp_should, running, superviser
 
     temp_is.value = get_temperature()
     temp_should.value = 40
-    running.value = True
-    superviser = multiprocessing.Process(target=supervise, args=(temp_is, temp_should, running))
+    running.value = False
 
-
-    superviser.start()
+    superviser = None
 
     app.run(host=host, port=port, debug=debug, use_reloader=False)
 
