0e1881da9bac0156efbee8127329d707a3768363
Hendrik Tjabben <tjabben@adobe.com>
2022-01-28 10:27:21 +0100

    Error handling in GUI

--- gui/gui.py
+++ gui/gui.py
@@ -3,45 +3,50 @@ import PySimpleGUI as sg
 import paramiko as pm
 import time
 from paramiko import BadHostKeyException, AuthenticationException
+from enum import Enum
 import yaml
 from os import path
 from sys import exit
 
-txt_color = "#282828"
-button_color = "#458588"
+TXT_COLOR = "#282828"
+BUTTON_COLOR = (TXT_COLOR, "#458588")
+FONT = ('Any 15')
+CONFIG_FILE = path.expanduser('~') + "/.heat_control_ssh.yml"
+INFO_COLOR = "#689d6a"
+WARNING_COLOR = "#d79921"
+ERROR_COLOR = "#cc241d"
 
-def create_popup(text):
+class CommandExecutionException(BaseException):
+    pass
 
-    bg_color = "#d79921"
+class PopupType(Enum):
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+
+def create_popup(text, type: PopupType):
+    popup_bg_color = INFO_COLOR if type is PopupType.INFO else WARNING_COLOR if type is PopupType.WARNING else ERROR_COLOR
 
     layout = [
-        [sg.Text(text, font=('Any 15'), background_color=bg_color, text_color=txt_color)],
+        [sg.Text(text, font=FONT, background_color=popup_bg_color, text_color=TXT_COLOR)],
         [sg.Column(
-            [[sg.Button('OK', size=(10, 1), font=('Any 15'), button_color=(txt_color, button_color))]],
-            justification='center')]
+            [[sg.Button('OK', size=(10, 1), font=FONT, button_color=BUTTON_COLOR)]], justification='center')]
     ]
     
-    window = sg.Window('INFO', layout, margins=(50, 25), background_color=bg_color)
-    
-    while True:
-        event, values = window.read()
-        if event == sg.WINDOW_CLOSED or event == "OK":
-            break
-
-    window.close()
+    popup_window = sg.Window(type.name, layout, margins=(50, 25), background_color=popup_bg_color)
+    popup_window.read()
+    popup_window.close()
 
 def parse_ssh_config():
-    home_dir = path.expanduser('~')
-    config_file = home_dir + "/.heat_control_ssh.yml"
     try:
-        with open(config_file, 'r') as config_file:
+        with open(CONFIG_FILE, 'r') as config_file:
             config = yaml.safe_load(config_file)
             host = config['host']
             port = config['port']
             username = config['username']
             password = config['password']
     except OSError:
-        create_popup("Cound not open/read ~/.heat_control_ssh.yml. Are you sure the file exists?")
+        create_popup("Cound not open/read " + CONFIG_FILE + ". Are you sure the file exists?", PopupType.ERROR)
         exit()
     return (host, port, username, password)
 
@@ -50,7 +55,6 @@ def execute_ssh_command(command):
     """
     https://www.ivankrizsan.se/2016/04/24/execute-shell-commands-over-ssh-using-python-and-paramiko/
     """
-    host, port, username, password = parse_ssh_config()
     ssh = None
 
     try:        
@@ -67,38 +71,48 @@ def execute_ssh_command(command):
 
         return stdout.channel.recv_exit_status()
     except BadHostKeyException:
-        create_popup("HeatControl server's host key cound not be verified")
+        raise CommandExecutionException("HeatControl server's host key cound not be verified")
     except AuthenticationException:
-        create_popup("Authenticating with HeatControl server failed. Wrong Username/Password?")
+        raise CommandExecutionException("Authenticating with HeatControl server failed. Wrong Username/Password?")
     except Exception:
-        create_popup("An unknown error occured while connecting to HeatControl server or executing a command")
+        raise CommandExecutionException("Error: Could not connect to Raspberry Pi \"" + username + "@" + host + "\"")
     finally:
         if ssh is not None:
             ssh.close()
 
 def start_service():
-    exit_code = execute_ssh_command("start_heat_control")
+    try:
+        exit_code = execute_ssh_command("start_heat_control")
+    except CommandExecutionException as e:
+        create_popup(e)
+
     if exit_code == 0:
-        create_popup("Started HeatControl")
+        create_popup("Started HeatControl", PopupType.INFO)
     elif exit_code == 1:
-        create_popup("Error: Could not start HeatControl. Server already running.")
+        create_popup("Could not start HeatControl. Server already running.", PopupType.WARNING)
     else:
-        create_popup("Error: Could not start HeatControl.")
+        create_popup("Could not start HeatControl.", PopupType.ERROR)
 
 def stop_service():
-    exit_code = execute_ssh_command("stop_heat_control")
+    try:
+        exit_code = execute_ssh_command("stop_heat_control")
+    except CommandExecutionException as e:
+        create_popup(e, PopupType.ERROR)
+    
     if exit_code == 0:
-        create_popup("Stopped HeatControl")
+        create_popup("Stopped HeatControl", PopupType.INFO)
     elif exit_code == 1:
-        create_popup("Error: Could not stop HeatControl. Server is not running.")
+        create_popup("Could not stop HeatControl. Server is not running.", PopupType.WARNING)
     else:
-        create_popup("Error: Could not stop HeatControl.")
+        create_popup("Error: Could not stop HeatControl.", PopupType.ERROR)
 
+
+host, port, username, password = parse_ssh_config()
 
 layout = [[
-        sg.Button("Start Service", size=(10, 2), font=('Any 15'), button_color=(txt_color, button_color)),
-        sg.Button("Stop Service",  size=(10, 2), font=('Any 15'), button_color=(txt_color, button_color))
-    ]]
+        sg.Button("Start Service", size=(10, 2), font=FONT, button_color=BUTTON_COLOR),
+        sg.Button("Stop Service",  size=(10, 2), font=FONT, button_color=BUTTON_COLOR)
+]]
 
 window = sg.Window(title="HeatControl", layout=layout, margins=(100, 50), background_color="#98971a").Finalize()
 
